4c4c7d0ec5d3ba790c2004bd71684af423c1994d
shumi.lg <shumi.lg@alibaba-inc.com>
2018-01-29 08:47:24 +0800

    Add unit test code.

--- tests/Switch.spec.js
+++ tests/Switch.spec.js
@@ -2,8 +2,14 @@ import expect from 'expect.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils, { Simulate } from 'react-addons-test-utils';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 import Switch from '../src';
 
-describe('Switch', () => {
+Enzyme.configure({ adapter: new Adapter() });
 
+describe('Switch', () => {
+  it ('should be render correctly', () => {
+    mount(<Switch />)
+  });
 });
