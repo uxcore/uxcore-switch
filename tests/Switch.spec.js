926624f92d0ae4fd001098be9d026f73ce23ef9e
shumi.lg <shumi.lg@alibaba-inc.com>
2018-06-25 03:03:58 +0800

    Adapt to react 16.

--- tests/Switch.spec.js
+++ tests/Switch.spec.js
@@ -1,9 +1,8 @@
 import expect from 'expect.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils, { Simulate } from 'react-addons-test-utils';
 import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 import Switch from '../src';
 
 Enzyme.configure({ adapter: new Adapter() });
